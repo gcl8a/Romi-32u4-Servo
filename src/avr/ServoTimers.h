be8ff60fb7b490f787dc02741e371649864452e0
Greg <lewin.gc@gmail.com>
2022-03-26 15:01:31 -0400

    deleted non-AVR stuff

--- src/avr/ServoTimers.h
+++ src/avr/ServoTimers.h
@@ -39,8 +39,8 @@
 typedef enum { _timer5, _timer1, _timer3, _timer4, _Nbr_16timers } timer16_Sequence_t;
 
 #elif defined(__AVR_ATmega32U4__)
-#define _useTimer1
-typedef enum { _timer1, _Nbr_16timers } timer16_Sequence_t;
+#define _useTimer3
+typedef enum { _timer3, _Nbr_16timers } timer16_Sequence_t;
 
 #elif defined(__AVR_AT90USB646__) || defined(__AVR_AT90USB1286__)
 #define _useTimer3
